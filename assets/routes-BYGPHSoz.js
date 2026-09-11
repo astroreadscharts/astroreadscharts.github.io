9ca48dc01c5e846eb2d78ab476d490753d54d484
Madduri Mahesh Vardhan <mvmahesh1998@gmail.com>
2026-09-12 01:38:21 +0530

    Updates

---
+++ assets/routes-BYGPHSoz.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-CyRaJvwK.js";var t=e();function n(){return(0,t.jsx)(`main`,{className:`h-screen overflow-hidden bg-background`,children:(0,t.jsx)(`iframe`,{title:`Astroreadscharts website`,src:`/astroreadscharts.html`,className:`h-full w-full border-0`})})}export{n as component};
